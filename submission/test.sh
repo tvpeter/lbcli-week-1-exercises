e890626b95b9fb3a7860b421a9172a670daf43b1
Vihiga Tyonum <withtvpeter@gmail.com>
2025-03-20 06:56:25 +0100

    add passing result to cmd calculator

--- submission/test.sh
+++ submission/test.sh
@@ -101,7 +101,7 @@ NEW_BALANCE=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getbalance)
 check_cmd "New balance check"
 echo "Your treasure balance: $NEW_BALANCE BTC"
 
-COLLECTED=$(echo "$NEW_BALANCE - $BALANCE")
+COLLECTED=$(echo "$NEW_BALANCE - $BALANCE" | bc)
 check_cmd "Balance calculation"
 echo "You've collected $COLLECTED BTC in treasures!"
 
